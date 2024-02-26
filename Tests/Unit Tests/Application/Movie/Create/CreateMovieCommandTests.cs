ece943e2f1328bf3679ab35cc9c743d80174c8c7
Faruk Aygun <farukaygun1@outlook.com>
2024-02-26 22:45:08 +0300

    Movie unit tests fix.

--- Tests/Unit Tests/Application/Movie/Create/CreateMovieCommandTests.cs
+++ Tests/Unit Tests/Application/Movie/Create/CreateMovieCommandTests.cs
@@ -3,7 +3,6 @@ using FluentAssertions;
 using Movie_Store_Web_API.Application.Movie_Operations.Create;
 using Movie_Store_Web_API.Db_Operations;
 using Movie_Store_Web_API.Entities;
-using System;
 using Unit_Tests.Test_Setup;
 
 namespace Unit_Tests.Application.Genre.Create
@@ -14,9 +13,8 @@ namespace Unit_Tests.Application.Genre.Create
 		private readonly IMapper mapper = fixture.Mapper;
 
 		[Fact]
-		public void WhenAlreadyExistMovieNameIsGiven_InvalidOperationException_ShouldBeReturned()
+		public async void WhenAlreadyExistMovieNameIsGiven_InvalidOperationException_ShouldBeReturned()
 		{
-			//Arrange
 			var movie = new Movie
 			{
 				Name = "Inception",
@@ -32,11 +30,10 @@ namespace Unit_Tests.Application.Genre.Create
 
 			var command = new CreateMovieCommand(context, mapper, mapper.Map<CreateMovieModel>(movie));
 
-
-			FluentActions
-				.Invoking(() => command.Handle())
+			await FluentActions
+				.Invoking(async () => await command.Handle())
 				.Should().ThrowAsync<InvalidOperationException>()
-				.WithMessage("Movie already exists!");
+				.WithMessage("Movie already exist!");
 		}
 	}
 }
